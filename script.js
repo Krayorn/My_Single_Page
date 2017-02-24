92d9f99022ef6248b88dd04fdeeb021a13a8df25
Nathael Arki <nathael.arki@supinternet.fr>
2017-02-24 17:14:31 +0100

    script ok no css

---
+++ script.js
@@ -0,0 +1,53 @@
+function isLoggedIn() {
+    if(sessionStorage.getItem('user')) {
+        return true;
+    }
+    return false;
+}
+
+function redirectIfLoggedIn(){
+    if(isLoggedIn()) {
+        $('#menu').show();
+        $('#user_name').html(sessionStorage.getItem('user'));
+        $('#loaded').html("");
+    } else {
+        $('#menu').hide();
+        $('#loaded').load("form.html form");
+    }
+}
+
+$(function(){
+    redirectIfLoggedIn();
+    $(document).on('submit','#login',function(){
+        sessionStorage.setItem('user', $(this).find('input[name="pseudo"]').val());
+        redirectIfLoggedIn();
+        return false;
+    });
+
+    $('#disconnected').click(function(){
+        sessionStorage.removeItem('user');
+        redirectIfLoggedIn();
+    });
+
+    $('#user_name').click(function(){
+        $('#loaded').load("user.html header", function(){
+            $('#user_pseudo').html(sessionStorage.getItem('user'));
+        });
+    });
+
+   $(document).on('click','#user_pseudo',function(){
+        $('#change_name').css('display', 'block');
+    });
+
+     $(document).on('submit','#change_name',function(){
+        sessionStorage.setItem('user', $(this).find('input[name="pseudo"]').val());
+        $('#user_name').html(sessionStorage.getItem('user'));
+        $('#user_pseudo').html(sessionStorage.getItem('user'));
+        $('#change_name').css('display', 'none');
+        return false;
+    });
+
+    $('#home').click(function(){
+        $('#loaded').html("");
+    });
+});
